754fbc52bcde4821a8327581f848f81749909f23
O-Tavares <114822357+O-Tavares@users.noreply.github.com>
2025-06-19 18:18:21 -0300

    gfd

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 let num = 0
 let memtrue = 0
-function mostrarMensagem() {
-  num++
-  document.title = `Ouro: ${num}`;
+let level = 1;
+function upgrade() {
+  if (num >= 10) {
+    num -= 10;
+    level++;
+    document.title = `Ouro: ${num}`;
+}}
+function Gaingold() {
+  num+
+  document.title == `Ouro: ${num}`;
   const titulo = document.querySelector("h1");
   titulo.textContent =  `${num}`;
 }
-
