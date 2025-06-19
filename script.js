99f7cf5d3aa7aa7fbd225c36445bfd1cc0510d1e
O-Tavares <114822357+O-Tavares@users.noreply.github.com>
2025-06-19 15:33:59 -0300

    ff

--- script.js
+++ script.js
@@ -4,5 +4,7 @@ function mostrarMensagem() {
   let message = `Você clicou no botão ${num} vez${num === 1 ? '' : 'es'}! 🎉`;
   alert(message);
   document.title = `Cliques: ${num}`;
+  const titulo = document.querySelector("h1");
+  titulo.textContent = `Você clicou ${num} vez${num === 1 ? '' : 'es'}!`;
 }
 
