50847c75f0873736bf8fb6887a03097eaf3ca560
O-Tavares <114822357+O-Tavares@users.noreply.github.com>
2025-06-19 15:44:37 -0300

    VV

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ let num = 0
 let memtrue = 0
 function mostrarMensagem() {
   num++
-  if (memtrue === 1);
-    let message = `Você clicou no botão ${num} vez${num === 1 ? '' : 'es'}! 🎉`;
-    alert(message);
   document.title = `Cliques: ${num}`;
   const titulo = document.querySelector("h1");
   titulo.textContent = `Você clicou ${num} vez${num === 1 ? '' : 'es'}!`;
