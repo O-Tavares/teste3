0409b35bdcf0b9d5cc667a1c691dabe3a7a25876
O-Tavares <114822357+O-Tavares@users.noreply.github.com>
2025-06-19 15:46:18 -0300

    vv

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let num = 0
 let memtrue = 0
 function mostrarMensagem() {
   num++
-  document.title = `Cliques: ${num}`;
+  document.title = `Ouro: ${num}`;
   const titulo = document.querySelector("h1");
-  titulo.textContent = `Você clicou ${num} vez${num === 1 ? '' : 'es'}!`;
+  titulo.textContent =  `${num}`;
 }
 
